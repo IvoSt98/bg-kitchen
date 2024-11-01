d66e71a20162b6dc1042070bd337bdd1a3336692
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-11-01 19:06:15 +0000

    Creating project

---
+++ bgkitchen/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bgkitchen project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bgkitchen.settings')
+
+application = get_wsgi_application()
