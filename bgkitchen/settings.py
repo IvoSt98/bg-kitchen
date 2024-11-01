1d59b12f5515db307bd027cd4385e97bf436dc4f
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-11-01 20:03:33 +0000

    Tring to fix a bug with the deployment

--- bgkitchen/settings.py
+++ bgkitchen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2f!e*i_rc7p2&zh1bk9)hm0j23n!wth&f7(lj#7)eq-%@*ssc8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ivost98-bgkitchen-e7uf23iwr4n.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ivost98-bgkitchen-e7uf23iwr4n.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
