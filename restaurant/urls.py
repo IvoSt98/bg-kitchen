51b97bb6a76c0e6c35173a2d226e1017cd9e18c0
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-11-16 15:10:52 +0000

    Were added 3 new templates about the reservations to be possible to make,cnage or view reservation. All of them was connected with the vies and urls, and in the end were connected with the base.html navbar

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='home'),
+    path('booking/', views.MakeReservation.as_view(), name='booking'),
+    path('view/', views.ViewReservation.as_view(), name='view'),
+    path('change/', views.ChangeReservation.as_view(), name='change'),
+
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from  django.views.generic  import TemplateView
 
 # Create your views here.
 class Index(TemplateView):
     template_name = 'reastaurant/index.html'
+
+class MakeReservation(TemplateView):
+    template_name = 'reastaurant/make_reservation.html'
+
+class ChangeReservation(TemplateView):
+    template_name = 'reastaurant/change_reservation.html'
+
+class ViewReservation(TemplateView):
+    template_name = 'reastaurant/view_reservation.html'
+
